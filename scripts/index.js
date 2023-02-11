4dd9dd7ea699661fcff1f2055c806b36a7ec4a99
ApostalDante <apostal666666@gmail.com>
2023-02-11 17:06:13 +0300

    hotfix/script-popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,34 @@
-const editProfiletButton = document.querySelector(".profile__edit-btm");
-const popup = document.querySelector(".popup");
-const formElement = document.querySelector(".form");
-const closePopupButton = popup.querySelector(".popup__close");
+const editProfiletButton = document.querySelector('.profile__edit-btm');
+const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.form');
+const closePopupButton = popup.querySelector('.popup__close');
+const userNameForm = document.querySelector('.form__user-name');
+const userMyselForm = document.querySelector('.form__user-myself');
+const userNameProfile = document.querySelector('.profile__user-name');
+const userMyselProfile = document.querySelector('.profile__user-myself');
 
-editProfiletButton.addEventListener("click", editProfiletButtonClick);
-popup.addEventListener("click", handleOverlyClick);
-closePopupButton.addEventListener("click", closePopupButtonClick);
-formElement.addEventListener('submit', handleFormSubmit);
+
+editProfiletButton.addEventListener('click', toggleOpenPopup);
+closePopupButton.addEventListener('click', closePopupButtonClick);
+formElement.addEventListener('submit', setUserFormProfile);
 
 function toggleOpenPopup() {
-  popup.classList.toggle("popup_opened");
-  getValuePlaceholder();
-}
-
-function editProfiletButtonClick() {
-  toggleOpenPopup();
+  popup.classList.toggle('popup_opened');
+  setFormValue();
 }
 
 function closePopupButtonClick() {
-  toggleOpenPopup();
+  popup.classList.remove('popup_opened');
 }
 
-function handleOverlyClick(event) {
-  if (event.target === event.currentTarget) {
-    toggleOpenPopup();
-  }
+function setFormValue() {
+  userNameForm.value = userNameProfile.textContent;
+  userMyselForm.value = userMyselProfile.textContent;
 }
 
-function cleanValueForm() {
-  let userName = document.querySelector(".form__user-name");
-  userName.value = "";
-  let userMysel = document.querySelector(".form__user-myself");
-  userMysel.value = "";
-}
-
-function changeValuePlaceholder(str) {
-  let userName = document.querySelector(".form__user-name");
-  let userMysel = document.querySelector(".form__user-myself");
-  cleanValueForm();
-  userName.placeholder = str;
-  userMysel.placeholder = str;
-}
-
-function getValuePlaceholder() {
-  let userName = document.querySelector(".form__user-name");
-  userName.placeholder = document.querySelector(".profile__user-name").textContent;
-  let userMysel = document.querySelector(".form__user-myself");
-  userMysel.placeholder = document.querySelector(".profile__user-myself").textContent;
-  cleanValueForm();
-}
-
-function sendingUserForm() {
-  let userNameValue = document.querySelector(".form__user-name").value;
-  let userMyselValue = document.querySelector(".form__user-myself").value;
-  if (userNameValue != '' && userMyselValue != '') {
-    if (userNameValue.length > 2 && userMyselValue.length > 2) {
-      document.querySelector(".profile__user-name").textContent = userNameValue;
-      document.querySelector(".profile__user-myself").textContent = userMyselValue;
-      closePopupButtonClick();
-    } else {
-      changeValuePlaceholder("В строках должно быть больше трёх символов");
-    }
-  } else {
-    changeValuePlaceholder("Строки должны быть заполнены");
-  }
-}
-
-function handleFormSubmit(evt) {
+function setUserFormProfile(evt) {
   evt.preventDefault();
-  sendingUserForm();
+  userNameProfile.textContent = userNameForm.value;
+  userMyselProfile.textContent = userMyselForm.value;
+  closePopupButtonClick();
 }
