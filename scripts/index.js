6f73abf7c98ae9060e6602ba0bccc2c02c73d310
ApostalDante <apostal666666@gmail.com>
2023-02-27 23:12:34 +0300

    hotfix/js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const page = document.querySelector('.page');
 const editProfiletButton = document.querySelector('.profile__edit-btm');
 const addCardButton = document.querySelector('.profile__add-btm');
-const popupAll = page.querySelectorAll('.popup');
 const popupUser = document.querySelector('.popup_type_user');
 const popupCard = document.querySelector('.popup_type_card');
 const formElementUser = document.querySelector('.form_type_user');
@@ -17,49 +16,37 @@ const cardElements = document.querySelector('.cards');
 const elementContainer = cardElements.querySelector('.elements');
 const popupImg = document.querySelector('.popup_type_img');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 function setUserFormValue() {
   userNameForm.value = userNameProfile.textContent;
   userMyselForm.value = userMyselProfile.textContent;
 }
 
-function toggleOpenPopupUser() {
-  popupUser.classList.toggle('popup_opened');
+function cleanCardFormValue() {
+  cardNameInput.value = '';
+  cardUrlInput.value = '';
+}
+
+function openPopupUser(popup) {
+  openPopup(popup);
   setUserFormValue();
 }
 
-function toggleOpenPopupCard() {
-  popupCard.classList.toggle('popup_opened');
+function openPopupCard(popup) {
+  openPopup(popup);
+  cleanCardFormValue();
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 function closePopupButtonAllClick() {
-  popupAll.forEach(pop => pop.classList.remove('popup_opened'))
+  const popup = document.querySelector('.popup_opened');
+  closePopup(popup);
 }
 
 
@@ -90,8 +77,12 @@ function addCard(cardName, cardUrl) {
   cardElement.querySelector('.element__img').src = cardUrl;
   likeButton.addEventListener('click', likeActive);
   trashButton.addEventListener('click', deleteCart);
-  elementContainer.prepend(cardElement);
-  imageClick.addEventListener('click', () => openPopupImg(cardElement));
+  imageClick.addEventListener('click', () => openPopupImg(cardName, cardUrl));
+  pushElementContainer(cardElement);
+}
+
+function pushElementContainer(element) {
+  elementContainer.prepend(element);
 }
 
 function deleteCart(evt) {
@@ -106,13 +97,12 @@ function cleanFormValue(input) {
   return input.value = '';
 }
 
-function openPopupImg(obg) {
-  const imgCard = obg.querySelector('.element__img');
-  const titleCard = obg.querySelector('.element__text');
+function openPopupImg(title, src) {
   const imgInPopup = document.querySelector('.popup__image');
   const titleInPopup = document.querySelector('.popup__title');
-  imgInPopup.src = imgCard.src;
-  titleInPopup.textContent = titleCard.textContent;
+  imgInPopup.src = src;
+  imgInPopup.alt = 'картинка';
+  titleInPopup.textContent = title;
   toggleOpenPopupImg();
 }
 
@@ -121,11 +111,9 @@ function toggleOpenPopupImg() {
 }
 
 initialCards.forEach(obg => addCard(obg.name, obg.link));
-editProfiletButton.addEventListener('click', toggleOpenPopupUser);
-addCardButton.addEventListener('click', toggleOpenPopupCard);
+editProfiletButton.addEventListener('click', () => openPopupUser(popupUser));
+addCardButton.addEventListener('click', () => openPopupCard(popupCard));
 formElementUser.addEventListener('submit', setUserFormProfile);
 formElementCard.addEventListener('submit', setCardFormProfile);
 closePopupButtonAll.forEach(btn => btn.addEventListener('click', () => closePopupButtonAllClick()));
-
-// плавность 
 
